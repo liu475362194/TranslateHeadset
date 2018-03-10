7e6fd2c2812531a5e58e18ce48e681a82c31b2d6
liu475362194 <475362194@qq.com>
2018-03-10 09:29:52 +0800

    添加说明层

---
+++ app/src/main/java/com/example/liu/translateheadset/util/ScreenUtils.java
@@ -0,0 +1,59 @@
+package com.willblaschko.android.alexavoicelibrary.utility;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+/**
+ * 屏幕帮助类
+ *
+ * Created by liu on 2018/3/9.
+ */
+
+public class ScreenUtils {
+    private static final String TAG = "ScreenUtils";
+
+    /**
+     * 获取屏幕宽度
+     * @param context
+     * @return
+     */
+//    @SuppressWarnings("deprecation")
+    public static int getScreenWidth(Context context){
+        DisplayMetrics dm = new DisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(dm);
+        Log.d(TAG, "获取到屏幕宽度为: " + dm.widthPixels);
+        return dm.widthPixels;
+//        return ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+//                .getWidth();
+
+    }
+
+    /**
+     * 获取屏幕高度
+     * @param context
+     * @return
+     */
+//    @SuppressWarnings("deprecation")
+    public static int getScreenHeight(Context context){
+        DisplayMetrics dm = new DisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(dm);
+        Log.d(TAG, "获取到屏幕高度为: " + dm.heightPixels);
+
+        //获取status_bar_height资源的ID
+        int statusBarHeight = 0;
+        int resourcesId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourcesId > 0){
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourcesId);
+            Log.d(TAG, "状态栏高度: " + statusBarHeight);
+        }
+        return dm.heightPixels - statusBarHeight;
+//        return ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+//                .getHeight();
+    }
+
+}
---
+++ app/src/main/java/com/example/liu/translateheadset/util/TeachView.java
@@ -0,0 +1,88 @@
+package com.willblaschko.android.alexavoicelibrary.utility;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.willblaschko.android.alexavoicelibrary.AlexaApplication;
+import com.willblaschko.android.alexavoicelibrary.R;
+
+/**
+ * 说明界面
+ * Created by liu on 2018/3/9.
+ */
+
+public class TeachView {
+
+    private static final Object mLock = new Object();
+    private static TeachView mInstance;
+    private Context context;
+
+    public TeachView setImageId(int imageId) {
+        this.imageId = imageId;
+        return mInstance;
+    }
+
+    private int imageId;
+
+    public TeachView(Context context) {
+        this.context = context;
+    }
+
+    public static TeachView getInstance(Context context) {
+        synchronized (mLock) {
+            if (mInstance == null) {
+                mInstance = new TeachView(context.getApplicationContext());
+            }
+            return mInstance;
+        }
+    }
+
+
+    //动态初始化图层
+    private ImageView initStudyImg(){
+        ImageView imageView = new ImageView(context);
+        imageView.setLayoutParams(new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setImageResource(imageId);
+        return imageView;
+    }
+
+    //配置LayoutParams参数
+    private WindowManager.LayoutParams setLayoutParams(){
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        //设置显示的类型，TYPE_PHONE指的是来电话的时候会被覆盖，其他时候会在最前端，显示位置在stateBar下面。
+        params.type = WindowManager.LayoutParams.TYPE_PHONE;
+        //设置显示格式
+        params.format = PixelFormat.RGBA_8888;
+        //设置对其方式
+        params.gravity = Gravity.LEFT | Gravity.TOP;
+        //设置宽高
+        params.width = ScreenUtils.getScreenWidth(context);
+        params.height = ScreenUtils.getScreenHeight(context);
+        return params;
+    }
+
+    //应用教程窗口
+    public void initStudyWindow(){
+        final WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        final ImageView imageView = initStudyImg();
+        windowManager.addView(imageView, setLayoutParams());
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                windowManager.removeView(imageView);
+            }
+        });
+    }
+}
